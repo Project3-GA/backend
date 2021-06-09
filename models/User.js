baaf5df05a6b5dd74eb15c078f20be3493c9b051
jkim057 <joshkim057@gmail.com>
2021-06-09 12:24:02 -0400

    Start User Authen

--- models/User.js
+++ models/User.js
@@ -13,17 +13,17 @@ const userSchema = new mongoose.Schema(
         required: true,
       },
     },
-    // {
-    //   timestamps: true,
-    //   toJSON: {
-    //     virtuals: true,
-    //     // ret is the returned Mongoose document
-    //     transform: (_doc, ret) => {
-    //       delete ret.password;
-    //       return ret;
-    //     },
-    //   },
-    // }
+    {
+      timestamps: true,
+      toJSON: {
+        virtuals: true,
+        // ret is the returned Mongoose document
+        transform: (_doc, ret) => {
+          delete ret.password;
+          return ret;
+        },
+      },
+    }
   );
 
 const User = mongoose.model('User', userSchema)
