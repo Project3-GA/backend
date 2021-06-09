6db828e87dbb4435aeb17e3935a70cafe34282c1
jkim057 <joshkim057@gmail.com>
2021-06-09 12:15:20 -0400

    Backend Connection

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+
+
+app.set("port", process.env.PORT || 3000);
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const userControllers = require('./controllers/userControllers')
+app.use('/api/users', userControllers)
+
+
+app.listen(app.get('port'), () => {
+	console.log(`✅ Listening on port ${app.get('port')}`);
+});
+
+module.exports = app
