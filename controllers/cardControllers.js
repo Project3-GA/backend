b1b282afe553674a281b8dfb35430067d868d1e8
jkim057 <joshkim057@gmail.com>
2021-06-11 13:50:34 -0400

    Setup Delete + patch Backend

--- controllers/cardControllers.js
+++ controllers/cardControllers.js
@@ -23,4 +23,29 @@ router.post('/', requireToken, (req, res, next) => {
 		.catch(next);
 });
 
+router.delete('/:id', requireToken, (req,res,next) =>{
+	const id = req.params.id
+	Card.findByIdAndDelete(id)
+		.then(() => res.status(204))
+		.catch(next);
+})
+
+// router.patch('/:id', requireToken, (req,res,next) =>{
+// 	const id = req.params.id
+// 	const updated = req.body
+// 	Card.findByIdAndUpdate(id, updated)
+// 		.then(())
+// })
+
+router.patch('/:id', (req, res, next) => {
+	// Grab the id
+	const id = req.params.id;
+	// Grab the data
+	const updated = req.body;
+	Card.findOneAndUpdate({ _id: id }, req.body, { new: true })
+		.then((card) => res.json(card))
+		.catch(next);
+});
+
+
 module.exports = router
